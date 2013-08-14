c8736dd00e7c1bf4cec1a91cfcb10ddcd77b2248
Karl-Aksel Puulmann <macobo@ut.ee>
2013-08-14 22:17:15 +0300

    Touched existing solution tester

--- grader/tests/solution_tester.py
+++ grader/tests/solution_tester.py
@@ -2,36 +2,34 @@ import unittest
 import os
 import importlib
 from grader import *
-import os
 
 CURRENT_FOLDER = os.path.dirname(__file__)
 
 # list of existing solution-tester pairs
-# (tester_module, solution_path)
+# (tester_module, solution_module)
 existing_tests = [
-    ("intress_tester",   "intress_solution.py"),
-    ("kypsisetort_test", "kypsisetort.py")
+    ("intress_tester",   "intress_solution"),
+    ("kypsisetort_test", "kypsisetort")
 ]
 
 def strip_extension(path):
     return os.path.splitext(path)[0]
 
 class Tests(unittest.TestCase):
-    def run_tester(self, tester_module, solution_path, working_dir=CURRENT_FOLDER):
+    def run_tester(self, tester_module, solution_module, working_dir=CURRENT_FOLDER):
         tester = importlib.import_module("grader.tests."+tester_module).t
 
-        tester.user_program_module = strip_extension(solution_path)
+        tester.user_program_module = solution_module
         tester.tester_module = tester_module
         tester.working_dir = working_dir
 
-        tester.testedProgramPath = strip_extension(solution_path)
         for test_name, (success, results) in tester.allTestResults():
             assert success, results["stderr"]
 
 
-def runner(tester, test_path):  
-    return lambda self: self.run_tester(tester, test_path)
+def runner(tester_module, solution_module):  
+    return lambda self: self.run_tester(tester_module, solution_module)
 
-for tester_path, solution_path in existing_tests:
-    setattr(Tests, "test_"+solution_path, 
-            runner(tester_path, solution_path))
+for tester_module, solution_module in existing_tests:
+    setattr(Tests, "test_"+solution_module, 
+            runner(tester_module, solution_module))
