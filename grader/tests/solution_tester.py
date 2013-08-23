ec2dabf4651bef32af65b21165523c05449c33e8
Karl-Aksel Puulmann <macobo@ut.ee>
2013-08-23 13:29:09 +0300

    read task info from file

--- grader/tests/solution_tester.py
+++ grader/tests/solution_tester.py
@@ -7,22 +7,19 @@ import os
 import sys
 import importlib
 import grader
+import json
 
 CURRENT_FOLDER = os.path.dirname(__file__)
 SOLUTION_FOLDER = os.path.join(os.path.dirname(os.path.dirname(CURRENT_FOLDER)), "tasks")
 
 sys.path.append(SOLUTION_FOLDER)
 
+tasks_json = json.loads(open(os.path.join(SOLUTION_FOLDER, "tasks.json")).read())
 # list of existing solution-tester pairs
 # (tester_module, solution_module)
 existing_tests = [
-    ("u2_intress_tester",        "u2_intress_solution"),
-    ("u2_kypsisetort_tester",    "u2_kypsisetort_solution"),
-    ("u2_intress_tester_other",  "u2_intress_solution"),
-    ("u2_nimed_tester",          "u2_nimed_solution"),
-    ("u12_tagurpidi_tester",     "u12_tagurpidi_solution"),
-    ("u6_taisnurkne_tester",     "u6_taisnurkne_solution")
-]
+    (task["tester"], task["solution"]) for unit in tasks_json.values() 
+                                       for task in unit.values()]
 
 class Tests(unittest.TestCase):
     def run_tester(self, tester_module, solution_module, working_dir=SOLUTION_FOLDER):
@@ -39,7 +36,7 @@ class Tests(unittest.TestCase):
             assert success, results["stderr"]
 
 
-def runner(tester_module, solution_module):  
+def runner(tester_module, solution_module):
     return lambda self: self.run_tester(tester_module, solution_module)
 
 for tester_module, solution_module in existing_tests:
