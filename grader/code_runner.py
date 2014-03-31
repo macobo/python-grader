78569f56488ef3e05907115e22a0d83d24995456
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2014-03-31 20:52:36 +0300

    Removed linux dependency on timeout cmd program. Should resolve #32

--- grader/code_runner.py
+++ grader/code_runner.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+import datetime
+import signal
+import time
 
 CURRENT_FOLDER = os.path.abspath(os.path.dirname(__file__))
 SANDBOX_DIR = os.path.join(os.path.dirname(CURRENT_FOLDER), "sandbox")
@@ -7,19 +10,43 @@ SANDBOX_DIR = os.path.join(os.path.dirname(CURRENT_FOLDER), "sandbox")
 TEST_RUN_CMD = os.path.join(SANDBOX_DIR, "run_test")
 DOCKER_SANDBOX = os.path.join(SANDBOX_DIR, 'run_tests_docker_sandbox')
 
-def call_command(cmd, cwd = None, decode=True):
-    if cwd is None:
-        cwd = os.getcwd()
-    subproc = subprocess.Popen(
-        cmd, cwd=cwd,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    stdout, stderr = subproc.communicate()
+
+def read_proc_results(proc, decode):
+    stdout = proc.stdout.read() 
+    stderr = proc.stderr.read()
     if decode:
         stdout = stdout.decode('utf-8')
         stderr = stderr.decode('utf-8')
+    status = proc.returncode
+    return status, stdout, stderr
 
-    status = subproc.returncode
+
+def call_command(cmd, timeout=float('inf'), cwd = None, decode=True, **subproc_options):
+    if cwd is None:
+        cwd = os.getcwd()
+
+    start = datetime.datetime.now()
+
+    subproc = subprocess.Popen(
+        cmd,
+        cwd=cwd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        **subproc_options
+    )
+
+    reached_timeout = False
+    while subproc.poll() is None:
+        time.sleep(0.02)
+        now = datetime.datetime.now()
+        if (now - start).microseconds >= timeout * 10**6:
+            os.kill(subproc.pid, signal.SIGKILL)
+            os.waitpid(-1, os.WNOHANG)
+            reached_timeout = True
+
+    status, stdout, stderr = read_proc_results(subproc, decode)
+    if reached_timeout:
+        status = 1
     return status, stdout, stderr
 
 
@@ -27,14 +54,12 @@ def call_test(test_name, tester_path, solution_path, options):
     # this assumes that tester, solution resides in the same path
     #working_dir = os.getcwd()#os.path.dirname(tester_path)
     cmd = [
-        "timeout",
-        str(options["timeout"]),
         TEST_RUN_CMD,
         tester_path,
         solution_path,
         test_name
     ]
-    status, stdout, stderr = call_command(cmd)
+    status, stdout, stderr = call_command(cmd, timeout=options["timeout"])
     return status == 0, stdout, stderr
 
 
