67fcd2b2eb495be506643d50b88e0c473a69c9b6
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2014-04-02 16:28:01 +0300

    Docker antics, tsts

--- tests/external_interface.py
+++ tests/external_interface.py
@@ -27,3 +27,15 @@ class Tests(unittest.TestCase):
         def empty(m): pass
         """), "invalid_syntax")
         assert results["success"], results
+
+    def test_docker_sandbox(self):
+        results = grader.test_code(dedent("""\
+        from grader import *
+        @test
+        def empty(m): pass
+        """), "", sandbox_cmd="docker")
+        if results["success"]:
+            assert results["success"], results
+        else:
+            # skip on filesystems without docker
+            self.assertIn("Invalid command: docker", results["reason"])
